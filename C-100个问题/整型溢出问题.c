6e2e5f33ef38ea185e4bb2173ada6bbd5c8988e6
alink777 <13630528+alink777@user.noreply.gitee.com>
2024-10-25 18:05:46 +0800

    2024/10/25

---
+++ C-100个问题/整型溢出问题.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char x;
+    x = (-100) + (-100);
+    printf("%d",x);
+    
+}
---
+++ C-100个问题/除法和取模的正负问题.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = 3, b=-2, c;
+    c = a / b;
+    printf("%d\n",c);
+    c = a % b;
+    printf("%d\n",c);
+}
